35e7409241914e7e97893a212f7523fd964c4de7
Tarasiuk Alexandr <alxn.newmax@yandex.ru>
2024-09-25 12:08:33 +0300

    f

--- docker-up.sh
+++ docker-up.sh
@@ -1,1 +1,13 @@
+#!/bin/sh
+
+# Create the docker-compose file
+echo "version: '3'" > google.txt
+echo "services:" >> google.txt
+echo "  my-service:" >> google.txt
+echo "    image: my-image:latest" >> google.txt
+echo "    ports:" >> google.txt
+echo "      - '8080:80'" >> google.txt
+
+
 docker-compose up -d
+
