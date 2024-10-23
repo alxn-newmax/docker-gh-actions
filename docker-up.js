dbf276e8cf7b71587a8d23dd7fa00399775a9321
Tarasiuk Alexandr <alxn.newmax@yandex.ru>
2024-10-23 15:18:37 +0300

    f

--- docker-up.js
+++ docker-up.js
@@ -2,25 +2,17 @@ import { exec } from 'child_process';
 
 // Функция для запуска docker-compose up
 function startDockerCompose() {
-    exec('docker login -u alxnnewmax -p dckr_pat_d411YDUBfm03_7ByJPy7AKuR-AY', (error, stdout, stderr) => {
+    
+    exec('docker-compose up -d', (error, stdout, stderr) => {
         if (error) {
-            console.error(`Docker не запущен или не установлен: ${error.message}`);
+            console.error(`Ошибка: ${error.message}`);
             return;
         }
-
+        if (stderr) {
+            console.error(`Ошибка: ${stderr}`);
+            return;
+        }
         console.log(`Вывод: ${stdout}`);
-
-        exec('docker-compose up -d', (error, stdout, stderr) => {
-            if (error) {
-                console.error(`Ошибка: ${error.message}`);
-                return;
-            }
-            if (stderr) {
-                console.error(`Ошибка: ${stderr}`);
-                return;
-            }
-            console.log(`Вывод: ${stdout}`);
-        });
     });
 }
 
