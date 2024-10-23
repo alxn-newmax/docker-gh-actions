9b77cf5ff580e7b828623275dcbb767806de8b11
Tarasiuk Alexandr <alxn.newmax@yandex.ru>
2024-10-23 12:29:04 +0300

    f

--- docker-up.js
+++ docker-up.js
@@ -1,4 +1,4 @@
-const { exec } = require('child_process');
+import { exec } from 'child_process';
 
 // Функция для запуска docker-compose up
 function startDockerCompose() {
--- index.js
+++ index.js
@@ -1,60 +1,47 @@
-// import cors from "cors";
-// import express from "express";
-// import router from "./routes/index.js";
-import { exec } from "child_process";
+import cors from "cors";
+import express from "express";
+import router from "./routes/index.js";
 
-exec('docker-compose up -d', (error, stdout, stderr) => {
-    if (error) {
-        console.error(`Ошибка: ${error.message}`);
-        return;
-    }
-    if (stderr) {
-        console.error(`Ошибка: ${stderr}`);
-        return;
-    }
-    console.log(`Вывод: ${stdout}`);
-});
+export class App {
+  constructor() {
+    this.app = express();
+    this.version = "/api";
+    this.env = process.env.NODE_ENV;
+    this.port = 4900;
+  }
 
-// export class App {
-//   constructor() {
-//     this.app = express();
-//     this.version = "/api";
-//     this.env = process.env.NODE_ENV;
-//     this.port = 4900;
-//   }
+  middlewares() {
+    this.app.use(
+      cors({
+        origin: "*",
+        methods: ["GET", "POST"],
+        credentials: true,
+      })
+    );
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
+  }
 
-//   middlewares() {
-//     this.app.use(
-//       cors({
-//         origin: "*",
-//         methods: ["GET", "POST"],
-//         credentials: true,
-//       })
-//     );
-//     this.app.use(express.json());
-//     this.app.use(express.urlencoded({ extended: true }));
-//   }
+  routes() {
+    this.app.get("/", function (req, res) {
+      res.sendFile("index.html", {
+        root: "./views",
+      });
+    });
+    this.app.use(this.version, router);
+  }
 
-//   routes() {
-//     this.app.get("/", function (req, res) {
-//       res.sendFile("index.html", {
-//         root: "./views",
-//       });
-//     });
-//     this.app.use(this.version, router);
-//   }
+  async run() {
+    const serverInfo = `Server is running on port: ${this.port}`;
+    this.app.listen(this.port, () => console.info(serverInfo));
+  }
 
-//   async run() {
-//     const serverInfo = `Server is running on port: ${this.port}`;
-//     this.app.listen(this.port, () => console.info(serverInfo));
-//   }
+  async main() {
+    this.middlewares();
+    this.routes();
+    await this.run();
+  }
+}
 
-//   async main() {
-//     this.middlewares();
-//     this.routes();
-//     await this.run();
-//   }
-// }
-
-// const app = new App();
-// await app.main();
+const app = new App();
+await app.main();
